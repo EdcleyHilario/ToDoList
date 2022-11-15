5a4436fada4705b1290c9fa33a1be00b43b50289
EdcleyHilario <edcleyhilario@gmail.com>
2022-11-15 11:34:04 -0300

    Update-URL Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css'
 export function Header(){
     return(
     <div className="header">
-      <img className='logo' src='/public/Logo.svg'/>
+      <img className='logo' src='/Logo.svg'/>
      </div>
     )
 }
